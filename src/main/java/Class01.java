a2dafef3d7975a801a3e4de7eaa7a5bdf6d6672d
Bengu Cambay <cambay.bengu@gmail.com>
2023-05-20 00:25:56 +0300

    Versiyon 2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -6,6 +6,10 @@ public class Class01 {
         int y = 2;
         System.out.println(x+y);
         System.out.println("Versiyon bir icin islemler tamam");
+        //Versiyon 2 calismalari
+
+        int z = 4;
+        System.out.println(x+y+z);
 
     }
 }
